93bc9638388367b181c4bae06c27abafd8f16ddd
DiegoUG <diego.uribe@globant.com>
2025-08-06 14:04:01 -0500

    [update] WIP

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // usa '/' si usarás dominio personalizado directo, no nombre de repo
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/',
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: 'index.html',
     },
-    assetsInlineLimit: 0,
-    chunkSizeWarningLimit: 1000,
   },
 });
